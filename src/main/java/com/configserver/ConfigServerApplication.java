919a4c57b936d42aeb30574001322168e3e7332a
sbagrec1 <shubhamcs@optum.com>
2021-03-09 16:13:30 +0530

    update config

--- src/main/java/com/configserver/ConfigServerApplication.java
+++ src/main/java/com/configserver/ConfigServerApplication.java
@@ -1,8 +1,14 @@
 package com.configserver;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @EnableConfigServer
 @SpringBootApplication
